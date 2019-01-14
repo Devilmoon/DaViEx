a03e4e3868ddc52e8b633aa410edd5d1d20a0d3b
Aristea <ibershimiaristea@yahoo.com>
2019-01-14 22:06:16 +0100

    clean out missing data from datasets, add 3rd dropdown variable

--- app1.py
+++ app1.py
@@ -8,7 +8,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-df = pd.read_csv('./static/data/all_2.csv')
+df = pd.read_csv('./static/data/all_3.csv')
 
 available_indicators = df['Indicator Name'].unique()
 
@@ -28,7 +28,7 @@ app.layout = html.Div([
                 labelStyle={'display': 'inline-block'}
             )
         ],
-        style={'width': '49%', 'display': 'inline-block'}),
+        style={'width': '33%', 'padding-right': '5px', 'display': 'inline-block'}),
 
         html.Div([
             dcc.Dropdown(
@@ -42,7 +42,21 @@ app.layout = html.Div([
                 value='Linear',
                 labelStyle={'display': 'inline-block'}
             )
-        ], style={'width': '49%', 'float': 'right', 'display': 'inline-block'})
+        ], style={'width': '33%', 'padding-right': '5px', 'display': 'inline-block'}), 
+
+        html.Div([
+            dcc.Dropdown(
+                id='crossfilter-zaxis-column',
+                options=[{'label': i, 'value': i} for i in available_indicators],
+                value='Life expectancy at birth, total (years)'
+            ),
+            dcc.RadioItems(
+                id='crossfilter-zaxis-type',
+                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
+                value='Linear',
+                labelStyle={'display': 'inline-block'}
+            )
+        ], style={'width': '33%', 'display': 'inline-block'}), 
     ], style={
         'borderBottom': 'thin lightgrey solid',
         'backgroundColor': 'rgb(250, 250, 250)',
@@ -58,6 +72,7 @@ app.layout = html.Div([
     html.Div([
         dcc.Graph(id='x-time-series'),
         dcc.Graph(id='y-time-series'),
+        dcc.Graph(id='z-time-series')
     ], style={'display': 'inline-block', 'width': '49%'}),
 
     html.Div(dcc.Slider(
@@ -74,11 +89,13 @@ app.layout = html.Div([
     dash.dependencies.Output('crossfilter-indicator-scatter', 'figure'),
     [dash.dependencies.Input('crossfilter-xaxis-column', 'value'),
      dash.dependencies.Input('crossfilter-yaxis-column', 'value'),
+     dash.dependencies.Input('crossfilter-zaxis-column', 'value'),
      dash.dependencies.Input('crossfilter-xaxis-type', 'value'),
      dash.dependencies.Input('crossfilter-yaxis-type', 'value'),
+     dash.dependencies.Input('crossfilter-zaxis-type', 'value'),
      dash.dependencies.Input('crossfilter-year--slider', 'value')])
-def update_graph(xaxis_column_name, yaxis_column_name,
-                 xaxis_type, yaxis_type,
+def update_graph(xaxis_column_name, yaxis_column_name, zaxis_column_name,
+                 xaxis_type, yaxis_type, zaxis_type,
                  year_value):
     dff = df[df['Year'] == year_value]
     traces = []
@@ -91,8 +108,10 @@ def update_graph(xaxis_column_name, yaxis_column_name,
             customdata=df_by_region[df_by_region['Indicator Name'] == yaxis_column_name]['Country Name'],
             mode='markers',
             marker={
-                'size': 15,
+                'sizemode': 'area', 
                 'opacity': 0.5,
+                'size': df_by_region[df_by_region['Indicator Name'] == zaxis_column_name]['Value'].values,
+                'sizemin': 10, 
                 'line': {'width': 0.5, 'color': 'white'}
             }, name=i
         ))
@@ -160,6 +179,15 @@ def update_x_timeseries(hoverData, yaxis_column_name, axis_type):
     dff = dff[dff['Indicator Name'] == yaxis_column_name]
     return create_time_series(dff, axis_type, yaxis_column_name)
 
+@app.callback(
+    dash.dependencies.Output('z-time-series', 'figure'),
+    [dash.dependencies.Input('crossfilter-indicator-scatter', 'hoverData'),
+     dash.dependencies.Input('crossfilter-zaxis-column', 'value'), 
+     dash.dependencies.Input('crossfilter-zaxis-type', 'value')])
+def update_z_timeseries(hoverData, zaxis_column_name, axis_type):
+    dff = df[df['Country Name'] == hoverData['points'][0]['customdata']]
+    dff = dff[dff['Indicator Name'] == zaxis_column_name]
+    return create_time_series(dff, axis_type, zaxis_column_name)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
