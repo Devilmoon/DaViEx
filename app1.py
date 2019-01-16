a4d4347627c83bcffb0c1b93969cd1ce5150433d
Aristea <ibershimiaristea@yahoo.com>
2019-01-16 01:33:57 +0100

    add checkbox in scatter plot

--- app1.py
+++ app1.py
@@ -19,7 +19,7 @@ app.layout = html.Div([
             dcc.Dropdown(
                 id='crossfilter-xaxis-column',
                 options=[{'label': i, 'value': i} for i in available_indicators],
-                value='Fertility rate, total (births per woman)'
+                value=''
             ),
             dcc.RadioItems(
                 id='crossfilter-xaxis-type',
@@ -28,13 +28,13 @@ app.layout = html.Div([
                 labelStyle={'display': 'inline-block'}
             )
         ],
-        style={'width': '33%', 'padding-right': '5px', 'display': 'inline-block'}),
+        style={'width': '49%', 'display': 'inline-block'}),
 
         html.Div([
             dcc.Dropdown(
                 id='crossfilter-yaxis-column',
                 options=[{'label': i, 'value': i} for i in available_indicators],
-                value='Life expectancy at birth, total (years)'
+                value=''
             ),
             dcc.RadioItems(
                 id='crossfilter-yaxis-type',
@@ -42,21 +42,18 @@ app.layout = html.Div([
                 value='Linear',
                 labelStyle={'display': 'inline-block'}
             )
-        ], style={'width': '33%', 'padding-right': '5px', 'display': 'inline-block'}), 
+        ], style={'width': '49%', 'float': 'right', 'display': 'inline-block'}),  
 
         html.Div([
-            dcc.Dropdown(
-                id='crossfilter-zaxis-column',
-                options=[{'label': i, 'value': i} for i in available_indicators],
-                value='Life expectancy at birth, total (years)'
-            ),
-            dcc.RadioItems(
-                id='crossfilter-zaxis-type',
-                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
-                value='Linear',
-                labelStyle={'display': 'inline-block'}
+            dcc.Checklist(
+                id='crossfilter-bubble-size',
+                options=[
+                    {'label': 'resize bubbles', 'value': 'show'}
+                ],
+                labelStyle={'display': 'inline-block'},
+                values=[]
             )
-        ], style={'width': '33%', 'display': 'inline-block'}), 
+        ])
     ], style={
         'borderBottom': 'thin lightgrey solid',
         'backgroundColor': 'rgb(250, 250, 250)',
@@ -72,7 +69,6 @@ app.layout = html.Div([
     html.Div([
         dcc.Graph(id='x-time-series'),
         dcc.Graph(id='y-time-series'),
-        dcc.Graph(id='z-time-series')
     ], style={'display': 'inline-block', 'width': '49%'}),
 
     html.Div(dcc.Slider(
@@ -89,18 +85,22 @@ app.layout = html.Div([
     dash.dependencies.Output('crossfilter-indicator-scatter', 'figure'),
     [dash.dependencies.Input('crossfilter-xaxis-column', 'value'),
      dash.dependencies.Input('crossfilter-yaxis-column', 'value'),
-     dash.dependencies.Input('crossfilter-zaxis-column', 'value'),
      dash.dependencies.Input('crossfilter-xaxis-type', 'value'),
      dash.dependencies.Input('crossfilter-yaxis-type', 'value'),
-     dash.dependencies.Input('crossfilter-zaxis-type', 'value'),
+     dash.dependencies.Input('crossfilter-bubble-size', 'values'), 
      dash.dependencies.Input('crossfilter-year--slider', 'value')])
-def update_graph(xaxis_column_name, yaxis_column_name, zaxis_column_name,
-                 xaxis_type, yaxis_type, zaxis_type,
+def update_graph(xaxis_column_name, yaxis_column_name,
+                 xaxis_type, yaxis_type, apply_bubble_size, 
                  year_value):
     dff = df[df['Year'] == year_value]
     traces = []
     for i in dff.Region.unique():
         df_by_region = dff[dff['Region'] == i]
+        bubble_size_variable = 'Renewable energy consumption (% of total final energy consumption)'
+        if apply_bubble_size:
+            bubble_size = df_by_region[df_by_region['Indicator Name'] == bubble_size_variable]['Value']
+        else: 
+            bubble_size = 15
         traces.append(go.Scatter(
             x=df_by_region[df_by_region['Indicator Name'] == xaxis_column_name]['Value'],
             y=df_by_region[df_by_region['Indicator Name'] == yaxis_column_name]['Value'],
@@ -110,8 +110,7 @@ def update_graph(xaxis_column_name, yaxis_column_name, zaxis_column_name,
             marker={
                 'sizemode': 'area', 
                 'opacity': 0.5,
-                'size': df_by_region[df_by_region['Indicator Name'] == zaxis_column_name]['Value'].values,
-                'sizemin': 10, 
+                'size': bubble_size,
                 'line': {'width': 0.5, 'color': 'white'}
             }, name=i
         ))
@@ -142,7 +141,7 @@ def create_time_series(dff, axis_type, title):
             mode='lines+markers'
         )],
         'layout': {
-            'height': 225,
+            'height': 240,
             'margin': {'l': 20, 'b': 30, 'r': 10, 't': 10},
             'annotations': [{
                 'x': 0, 'y': 0.85, 'xanchor': 'left', 'yanchor': 'bottom',
@@ -178,16 +177,6 @@ def update_x_timeseries(hoverData, yaxis_column_name, axis_type):
     dff = df[df['Country Name'] == hoverData['points'][0]['customdata']]
     dff = dff[dff['Indicator Name'] == yaxis_column_name]
     return create_time_series(dff, axis_type, yaxis_column_name)
-
-@app.callback(
-    dash.dependencies.Output('z-time-series', 'figure'),
-    [dash.dependencies.Input('crossfilter-indicator-scatter', 'hoverData'),
-     dash.dependencies.Input('crossfilter-zaxis-column', 'value'), 
-     dash.dependencies.Input('crossfilter-zaxis-type', 'value')])
-def update_z_timeseries(hoverData, zaxis_column_name, axis_type):
-    dff = df[df['Country Name'] == hoverData['points'][0]['customdata']]
-    dff = dff[dff['Indicator Name'] == zaxis_column_name]
-    return create_time_series(dff, axis_type, zaxis_column_name)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
